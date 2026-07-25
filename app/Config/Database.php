d2134e5d78d81754fe65a1f58f1fe9cdc8cbcd23
Henrique <henrique@risecrm.com>
2026-07-25 21:12:43 +0000

    fix: remover __construct duplicado no Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -93,19 +93,4 @@ class Database extends Config
 
 	//--------------------------------------------------------------------
 
-	public function __construct()
-	{
-		parent::__construct();
-
-		// Ensure that we always set the database group to 'tests' if
-		// we are currently running an automated test suite, so that
-		// we don't overwrite live data on accident.
-		if (ENVIRONMENT === 'testing')
-		{
-			$this->defaultGroup = 'tests';
-		}
-	}
-
-	//--------------------------------------------------------------------
-
 }
