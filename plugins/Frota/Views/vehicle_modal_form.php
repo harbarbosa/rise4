b223a05c3d4013f4daeef35cc435ece060b24703
Henrique Barbosa <67531625+harbarbosa@users.noreply.github.com>
2026-09-05 02:11:16 -0300

    feat(Frota): integrar placa, ano, km e FIPE no veiculo

--- plugins/Frota/Views/vehicle_modal_form.php
+++ plugins/Frota/Views/vehicle_modal_form.php
@@ -3,36 +3,114 @@
     <div class="container-fluid">
         <input type="hidden" name="id" value="<?php echo $model_info->id ?? ''; ?>" />
 
-        <?php
-        $fields = [
-            ['plate', 'Placa', $model_info->plate ?? '', true],
-            ['prefix', 'Prefixo', $model_info->prefix ?? '', false],
-            ['make', 'Marca', $model_info->make ?? '', false],
-            ['model', 'Modelo', $model_info->model ?? '', true],
-            ['year', 'Ano', $model_info->year ?? '', false],
-            ['current_odometer', 'KM atual', $model_info->current_odometer ?? '', false],
-            ['next_service_odometer', 'Próxima revisão (km)', $model_info->next_service_odometer ?? '', false],
-        ];
-        foreach ($fields as $field) {
-            [$name, $label, $value, $required] = $field;
-        ?>
-            <div class="form-group">
-                <div class="row">
-                    <label for="<?php echo $name; ?>" class="col-md-3"><?php echo $label; ?></label>
-                    <div class="col-md-9">
-                        <?php echo form_input([
-                            'id' => $name,
-                            'name' => $name,
-                            'value' => $value,
-                            'class' => 'form-control',
-                            'placeholder' => $label,
-                            'data-rule-required' => $required ? true : null,
-                            'data-msg-required' => $required ? app_lang('field_required') : null,
-                        ]); ?>
-                    </div>
+        <div class="form-group">
+            <div class="row">
+                <label for="plate" class="col-md-3">Placa</label>
+                <div class="col-md-9">
+                    <?php echo form_input([
+                        'id' => 'plate',
+                        'name' => 'plate',
+                        'value' => $model_info->plate ?? '',
+                        'class' => 'form-control frota-plate-mask',
+                        'placeholder' => 'ABC-1D23',
+                        'maxlength' => 8,
+                        'data-rule-required' => true,
+                        'data-rule-pattern' => '^[A-Z]{3}-[0-9][A-Z][0-9]{2}$',
+                        'data-msg-required' => app_lang('field_required'),
+                        'data-msg-pattern' => 'Use o formato ABC-1D23.'
+                    ]); ?>
+                    <div class="text-off small mt-1">Formato Mercosul: ABC-1D23</div>
                 </div>
             </div>
-        <?php } ?>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <label for="prefix" class="col-md-3">Prefixo</label>
+                <div class="col-md-9">
+                    <?php echo form_input([
+                        'id' => 'prefix',
+                        'name' => 'prefix',
+                        'value' => $model_info->prefix ?? '',
+                        'class' => 'form-control',
+                        'placeholder' => 'Prefixo'
+                    ]); ?>
+                </div>
+            </div>
+        </div>
+
+        <input type="hidden" name="make" id="make" value="<?php echo esc($model_info->make ?? ''); ?>" />
+        <div class="form-group">
+            <div class="row">
+                <label for="make_code" class="col-md-3">Marca</label>
+                <div class="col-md-9">
+                    <select id="make_code" class="form-control"></select>
+                    <div class="text-off small mt-1">Marcas consultadas na Tabela FIPE via BrasilAPI. Também é possível digitar manualmente.</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <label for="model" class="col-md-3">Modelo</label>
+                <div class="col-md-9">
+                    <select id="model" name="model" class="form-control validate-hidden" data-rule-required="true" data-msg-required="<?php echo app_lang('field_required'); ?>">
+                        <?php if (!empty($model_info->model)) { ?>
+                            <option value="<?php echo esc($model_info->model); ?>" selected><?php echo esc($model_info->model); ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <label for="year" class="col-md-3">Ano</label>
+                <div class="col-md-9">
+                    <?php echo form_input([
+                        'id' => 'year',
+                        'name' => 'year',
+                        'value' => $model_info->year ?? '',
+                        'class' => 'form-control frota-year-mask',
+                        'placeholder' => '2026',
+                        'maxlength' => 4,
+                        'inputmode' => 'numeric'
+                    ]); ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <label for="current_odometer" class="col-md-3">KM atual</label>
+                <div class="col-md-9">
+                    <?php echo form_input([
+                        'id' => 'current_odometer',
+                        'name' => 'current_odometer',
+                        'value' => $model_info->current_odometer ?? '',
+                        'class' => 'form-control frota-km-mask',
+                        'placeholder' => '111.111.111',
+                        'inputmode' => 'numeric'
+                    ]); ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <label for="next_service_odometer" class="col-md-3">Próxima revisão (km)</label>
+                <div class="col-md-9">
+                    <?php echo form_input([
+                        'id' => 'next_service_odometer',
+                        'name' => 'next_service_odometer',
+                        'value' => $model_info->next_service_odometer ?? '',
+                        'class' => 'form-control frota-km-mask',
+                        'placeholder' => '111.111.111',
+                        'inputmode' => 'numeric'
+                    ]); ?>
+                </div>
+            </div>
+        </div>
 
         <div class="form-group">
             <div class="row">
@@ -79,6 +157,20 @@
 
 <script type="text/javascript">
     $(document).ready(function () {
+        if (window.FrotaUI) {
+            window.FrotaUI.prepareMasks('#frota-vehicle-form');
+            window.FrotaUI.initVehicleFipe({
+                brandSelector: '#make_code',
+                makeHiddenSelector: '#make',
+                modelSelector: '#model',
+                currentMake: <?php echo json_encode($model_info->make ?? ''); ?>,
+                currentModel: <?php echo json_encode($model_info->model ?? ''); ?>,
+                type: 'carros',
+                brandsUrl: <?php echo json_encode(get_uri('frota/fipe/marcas')); ?>,
+                modelsUrl: <?php echo json_encode(get_uri('frota/fipe/modelos')); ?>
+            });
+        }
+
         $("#frota-vehicle-form").appForm({
             onSuccess: function () { location.reload(); }
         });
