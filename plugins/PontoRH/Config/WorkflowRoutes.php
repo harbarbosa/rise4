7a4c4221623ae11233bf8021c5928f24a1096ec7
Henrique Barbosa <67531625+harbarbosa@users.noreply.github.com>
2026-09-05 00:59:08 -0300

    refactor(PontoRH): consolidar rotas do novo fluxo de tratamento

--- plugins/PontoRH/Config/WorkflowRoutes.php
+++ plugins/PontoRH/Config/WorkflowRoutes.php
@@ -6,7 +6,12 @@ use Config\Services;
 
 $routes = Services::routes();
 
-// These routes intentionally override the generic write routes registered in Routes.php.
+// Estas rotas sobrescrevem intencionalmente as rotas genéricas de Routes.php.
+$routes->get('pontorh/tratamento', 'PontoRH_treatment_dashboard::index', ['namespace' => 'PontoRH\Controllers']);
+$routes->post('pontorh/tratamento/list_data', 'PontoRH_treatment_ui::list_data', ['namespace' => 'PontoRH\Controllers']);
+$routes->post('pontorh/tratamento/detalhes', 'PontoRH_treatment_ui::details', ['namespace' => 'PontoRH\Controllers']);
+$routes->get('pontorh/tratamento/detalhes/(:num)', 'PontoRH_treatment_ui::details/$1', ['namespace' => 'PontoRH\Controllers']);
+
 $routes->post('pontorh/registros/save', 'PontoRH_records_guarded::save', ['namespace' => 'PontoRH\Controllers']);
 $routes->post('pontorh/registros/delete', 'PontoRH_records_guarded::delete', ['namespace' => 'PontoRH\Controllers']);
 $routes->post('pontorh/tratamento/save_manual', 'PontoRH_treatment_guarded::save_manual', ['namespace' => 'PontoRH\Controllers']);
