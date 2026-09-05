3a2ffa782ce77aac693689682ad5b3639718a57e
Henrique Barbosa <67531625+harbarbosa@users.noreply.github.com>
2026-09-05 01:44:25 -0300

    fix(Frota): adicionar traducoes pt-BR dos menus

---
+++ plugins/Frota/Language/portuguese/default_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['frota'] = 'Gestão de Frota';
+$lang['frota_dashboard'] = 'Visão geral';
+$lang['frota_vehicles'] = 'Veículos';
+$lang['frota_fuelings'] = 'Abastecimentos';
+$lang['frota_maintenances'] = 'Manutenções';
+$lang['frota_issues'] = 'Ocorrências';
+$lang['frota_new_vehicle'] = 'Novo veículo';
+$lang['frota_new_fueling'] = 'Novo abastecimento';
+$lang['frota_new_maintenance'] = 'Nova manutenção';
+$lang['frota_register_issue'] = 'Registrar problema';
