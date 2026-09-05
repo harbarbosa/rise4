e1484bd51e98ae704d81bed8083f4d91db2ee107
Henrique Barbosa <67531625+harbarbosa@users.noreply.github.com>
2026-09-05 00:10:10 -0300

    PontoRH: padronizar tela de registros

--- plugins/PontoRH/Views/records/index.php
+++ plugins/PontoRH/Views/records/index.php
@@ -3,77 +3,66 @@ $default_date_from = get_my_local_time('Y-m-01');
 $default_date_to = get_my_local_time('Y-m-t');
 ?>
 
-<div id="page-content" class="page-wrapper clearfix">
+<style>
+    .pontorh-records-index .filter-label {font-size:12px;color:#69727d;margin-bottom:5px;}
+    .pontorh-records-index #pontorh-records-table td {vertical-align:middle;}
+    .pontorh-records-index #pontorh-records-table td:nth-child(1),
+    .pontorh-records-index #pontorh-records-table td:nth-child(2),
+    .pontorh-records-index #pontorh-records-table td:nth-child(3),
+    .pontorh-records-index #pontorh-records-table td:nth-child(7),
+    .pontorh-records-index #pontorh-records-table td:nth-child(8) {white-space:nowrap;}
+</style>
+
+<div id="page-content" class="page-wrapper clearfix pontorh-records-index">
     <div class="card">
         <div class="page-title clearfix">
-            <h1><?php echo app_lang('pontorh_records'); ?></h1>
+            <div>
+                <h1>Registros de Ponto</h1>
+                <div class="text-muted">Consulte as marcações registradas pelos funcionários.</div>
+            </div>
             <div class="title-button-group">
                 <?php if ($can_manage) { ?>
-                    <?php echo modal_anchor(get_uri('pontorh/registros/modal_form'), "<i data-feather='plus-circle' class='icon-16'></i> " . app_lang('pontorh_records'), array('class' => 'btn btn-primary', 'title' => app_lang('pontorh_records'))); ?>
+                    <?php echo modal_anchor(get_uri('pontorh/registros/modal_form'), "<i data-feather='plus' class='icon-16'></i> Nova marcação", array('class' => 'btn btn-primary', 'title' => 'Nova marcação')); ?>
                 <?php } ?>
             </div>
         </div>
 
         <div class="card-body border-bottom">
-            <div class="row g-2 align-items-end">
-                <div class="col-md-3">
-                    <label class="form-label"><?php echo app_lang('pontorh_employee'); ?></label>
-                    <select name="team_member_id" id="pontorh-filter-team-member" class="form-control select2" style="width:100%;">
+            <div class="row align-items-end">
+                <div class="col-lg-3 col-md-6">
+                    <label class="filter-label">Funcionário</label>
+                    <select name="team_member_id" id="pontorh-filter-team-member" class="form-control select2 w100p">
                         <option value=""></option>
                         <?php foreach ($team_members_dropdown as $value => $label) { ?>
                             <option value="<?php echo esc($value); ?>"><?php echo esc($label); ?></option>
                         <?php } ?>
                     </select>
                 </div>
-                <div class="col-md-3">
-                    <label class="form-label"><?php echo app_lang('start_date'); ?></label>
-                    <div class="input-group">
-                        <?php echo form_input(array(
-                            'id' => 'pontorh-filter-date-from',
-                            'type' => 'text',
-                            'class' => 'form-control datepicker',
-                            'autocomplete' => 'off',
-                            'placeholder' => app_lang('from_date'),
-                            'value' => $default_date_from,
-                        )); ?>
-                        <span class="input-group-text"><i data-feather="calendar" class="icon-14"></i></span>
-                    </div>
+                <div class="col-lg-2 col-md-6">
+                    <label class="filter-label">Data inicial</label>
+                    <?php echo form_input(array('id' => 'pontorh-filter-date-from','type' => 'text','class' => 'form-control datepicker','autocomplete' => 'off','value' => $default_date_from)); ?>
                 </div>
-                <div class="col-md-3">
-                    <label class="form-label"> <?php echo app_lang('end_date'); ?></label>
-                    <div class="input-group">
-                        <?php echo form_input(array(
-                            'id' => 'pontorh-filter-date-to',
-                            'type' => 'text',
-                            'class' => 'form-control datepicker',
-                            'autocomplete' => 'off',
-                            'placeholder' => app_lang('to_date'),
-                            'value' => $default_date_to,
-                        )); ?>
-                        <span class="input-group-text"><i data-feather="calendar" class="icon-14"></i></span>
-                    </div>
+                <div class="col-lg-2 col-md-6">
+                    <label class="filter-label">Data final</label>
+                    <?php echo form_input(array('id' => 'pontorh-filter-date-to','type' => 'text','class' => 'form-control datepicker','autocomplete' => 'off','value' => $default_date_to)); ?>
                 </div>
-                <div class="col-md-3">
-                    <label class="form-label"><?php echo app_lang('pontorh_status'); ?></label>
-                    <select name="status" id="pontorh-filter-status" class="form-control select2" style="width:100%;">
+                <div class="col-lg-2 col-md-6">
+                    <label class="filter-label">Marcação</label>
+                    <select name="punch_type" id="pontorh-filter-type" class="form-control select2 w100p">
                         <option value=""></option>
-                        <?php foreach ($status_dropdown as $value => $label) { ?>
-                            <option value="<?php echo esc($value); ?>"><?php echo esc($label); ?></option>
-                        <?php } ?>
+                        <?php foreach ($punch_type_dropdown as $value => $label) { ?><option value="<?php echo esc($value); ?>"><?php echo esc($label); ?></option><?php } ?>
                     </select>
                 </div>
-                <div class="col-md-3">
-                    <label class="form-label"><?php echo app_lang('pontorh_type'); ?></label>
-                    <select name="punch_type" id="pontorh-filter-type" class="form-control select2" style="width:100%;">
+                <div class="col-lg-3 col-md-6">
+                    <label class="filter-label">Situação</label>
+                    <select name="status" id="pontorh-filter-status" class="form-control select2 w100p">
                         <option value=""></option>
-                        <?php foreach ($punch_type_dropdown as $value => $label) { ?>
-                            <option value="<?php echo esc($value); ?>"><?php echo esc($label); ?></option>
-                        <?php } ?>
+                        <?php foreach ($status_dropdown as $value => $label) { ?><option value="<?php echo esc($value); ?>"><?php echo esc($label); ?></option><?php } ?>
                     </select>
                 </div>
-                <div class="col-md-12">
-                    <button type="button" id="pontorh-filter-btn" class="btn btn-primary btn-sm me-2"><?php echo app_lang('filter'); ?></button>
-                    <button type="button" id="pontorh-clear-btn" class="btn btn-default btn-sm"><?php echo app_lang('clear'); ?></button>
+                <div class="col-12 mt-3">
+                    <button type="button" id="pontorh-filter-btn" class="btn btn-primary"><i data-feather="filter" class="icon-16"></i> Filtrar</button>
+                    <button type="button" id="pontorh-clear-btn" class="btn btn-default">Limpar</button>
                 </div>
             </div>
         </div>
@@ -105,27 +94,22 @@ $default_date_to = get_my_local_time('Y-m-t');
     $(document).ready(function () {
         $(".page-wrapper .select2").select2();
         setDatePicker("#pontorh-filter-date-from, #pontorh-filter-date-to");
-
         $("#pontorh-records-table").appTable({
             source: "<?php echo_uri('pontorh/registros/list_data'); ?>",
             filterParams: $.extend({datatable: true}, pontorhRecordsFilters()),
             order: [[1, "desc"]],
             columns: [
-                {title: "<?php echo app_lang('pontorh_employee'); ?>"},
-                {title: "<?php echo app_lang('date'); ?>"},
-                {title: "<?php echo app_lang('time'); ?>"},
-                {title: "<?php echo app_lang('pontorh_type'); ?>"},
-                {title: "<?php echo app_lang('pontorh_location'); ?>"},
-                {title: "<?php echo app_lang('pontorh_source'); ?>"},
-                {title: "<?php echo app_lang('pontorh_status'); ?>"},
-                {title: "<i data-feather='menu' class='icon-16'></i>", "class": "text-center option w140"}
+                {title: "Funcionário"},
+                {title: "Data", "class": "w100"},
+                {title: "Hora", "class": "w90"},
+                {title: "Marcação"},
+                {title: "Local"},
+                {title: "Origem"},
+                {title: "Situação"},
+                {title: "<i data-feather='menu' class='icon-16'></i>", "class": "text-center option w100"}
             ]
         });
-
-        $("#pontorh-filter-btn").on("click", function () {
-            reloadPontorhRecordsTable();
-        });
-
+        $("#pontorh-filter-btn").on("click", reloadPontorhRecordsTable);
         $("#pontorh-clear-btn").on("click", function () {
             $("#pontorh-filter-team-member, #pontorh-filter-status, #pontorh-filter-type").val("").trigger("change");
             $("#pontorh-filter-date-from, #pontorh-filter-date-to").val("");
