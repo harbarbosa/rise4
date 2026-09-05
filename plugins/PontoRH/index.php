4efd4f3a525022665dfa5bdc4106e8c2bc2c82d9
Henrique Barbosa <67531625+harbarbosa@users.noreply.github.com>
2026-09-05 02:23:33 -0300

    refactor(PontoRH): desativar fluxo legado e ativar monitor Solides

--- plugins/PontoRH/index.php
+++ plugins/PontoRH/index.php
@@ -1,69 +1,36 @@
 <?php
 
 defined('PLUGINPATH') or exit('No direct script access allowed');
-
 /*
-  Plugin Name: Ponto RH
-  Description: Modulo de controle de ponto integrado ao RiseCRM.
-  Version: 0.2.0
-  Requires at least: 3.9.0
-  Author: Internal
+ Plugin Name: Ponto RH
+ Description: Monitor de ponto integrado à Sólides para DP e funcionários.
+ Version: 0.3.0
+ Requires at least: 3.9.0
+ Author: Internal
 */
-
-require_once __DIR__ . '/Helpers/pontorh_helper.php';
-require_once __DIR__ . '/Plugin.php';
-
-$pontorh_language = get_setting('language') ?: 'english';
-$pontorh_language_file = __DIR__ . '/Language/' . $pontorh_language . '/default_lang.php';
-if (file_exists($pontorh_language_file)) {
-    require_once $pontorh_language_file;
-} elseif (file_exists(__DIR__ . '/Language/english/default_lang.php')) {
-    require_once __DIR__ . '/Language/english/default_lang.php';
-}
-$pontorh_closing_language_file = __DIR__ . '/Language/' . $pontorh_language . '/closing_lang.php';
-if (file_exists($pontorh_closing_language_file)) {
-    require_once $pontorh_closing_language_file;
-} elseif (file_exists(__DIR__ . '/Language/english/closing_lang.php')) {
-    require_once __DIR__ . '/Language/english/closing_lang.php';
-}
-
+require_once __DIR__.'/Helpers/pontorh_helper.php';
+require_once __DIR__.'/Plugin.php';
+$pontorh_language=get_setting('language')?:'english';
+$pontorh_language_file=__DIR__.'/Language/'.$pontorh_language.'/default_lang.php';
+if(file_exists($pontorh_language_file))require_once $pontorh_language_file;elseif(file_exists(__DIR__.'/Language/english/default_lang.php'))require_once __DIR__.'/Language/english/default_lang.php';
 \PontoRH\Plugin::register();
 
-app_hooks()->add_action('app_hook_head_extension', function () {
-    load_js(array('plugins/PontoRH/assets/js/status_badges.js'));
+// Substitui o menu antigo de registro/tratamento/fechamento por uma camada de monitoramento.
+app_hooks()->add_filter('app_filter_staff_left_menu',function($menu){
+    if(isset($menu['pontorh'])){
+        $menu['pontorh']['name']='Ponto · Sólides';
+        $menu['pontorh']['url']='pontorh';
+        $menu['pontorh']['class']='clock';
+        $menu['pontorh']['submenu']=array('pontorh_solides'=>array('name'=>'Monitor Sólides','url'=>'pontorh','class'=>'activity'));
+        $menu['pontorh']['sub_pages']=array('pontorh/index','pontorh/solides');
+    }
+    return $menu;
 });
 
-if (file_exists(__DIR__ . '/Hooks/closing_menu.php')) {
-    require_once __DIR__ . '/Hooks/closing_menu.php';
-}
-
-if (file_exists(__DIR__ . '/Config/Routes.php')) {
-    require_once __DIR__ . '/Config/Routes.php';
-}
-if (file_exists(__DIR__ . '/Config/WorkflowRoutes.php')) {
-    require_once __DIR__ . '/Config/WorkflowRoutes.php';
-}
-
-register_installation_hook('PontoRH', function () {
-    require_once __DIR__ . '/install.php';
-    \PontoRH\install\pontorh_install();
-});
-
-register_activation_hook('PontoRH', function () {
-    \PontoRH\Plugin::runMigrations();
-});
-
-register_update_hook('PontoRH', function () {
-    require_once __DIR__ . '/install.php';
-    \PontoRH\install\pontorh_install();
-    \PontoRH\Plugin::runMigrations();
-});
-
-register_deactivation_hook('PontoRH', function () {
-    return true;
-});
-
-register_uninstallation_hook('PontoRH', function () {
-    require_once __DIR__ . '/uninstall.php';
-    \PontoRH\install\pontorh_uninstall();
-});
+if(file_exists(__DIR__.'/Config/Routes.php'))require_once __DIR__.'/Config/Routes.php';
+// WorkflowRoutes legado não é carregado: ajustes, marcações e fechamento permanecem exclusivamente na Sólides.
+register_installation_hook('PontoRH',function(){require_once __DIR__.'/install.php';\PontoRH\install\pontorh_install();});
+register_activation_hook('PontoRH',function(){\PontoRH\Plugin::runMigrations();});
+register_update_hook('PontoRH',function(){require_once __DIR__.'/install.php';\PontoRH\install\pontorh_install();\PontoRH\Plugin::runMigrations();});
+register_deactivation_hook('PontoRH',function(){return true;});
+register_uninstallation_hook('PontoRH',function(){require_once __DIR__.'/uninstall.php';\PontoRH\install\pontorh_uninstall();});
