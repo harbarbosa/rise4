95a4f68aa375ada5e9749b3d7000dd7f5d39af04
Henrique Barbosa <67531625+harbarbosa@users.noreply.github.com>
2026-09-05 00:31:08 -0300

    fix(PontoRH): evitar conflito Select2 ao editar marcacao

--- plugins/PontoRH/Views/treatment/record_modal.php
+++ plugins/PontoRH/Views/treatment/record_modal.php
@@ -37,106 +37,64 @@ $selected_notes = (string) ($record->notes ?? '');
             <div class="text-muted mb-2"><?php echo app_lang('pontorh_record_action_delete'); ?></div>
             <div class="alert alert-warning mb-3"><?php echo app_lang('pontorh_record_action_delete_help'); ?></div>
         <?php } else { ?>
-            <div class="form-group">
-                <div class="row">
-                    <label for="team_member_id" class="col-md-3"><?php echo app_lang('pontorh_employee'); ?></label>
-                    <div class="col-md-9">
-                        <?php echo form_hidden('team_member_id', $selected_team_member_id); ?>
-                        <?php echo form_dropdown('team_member_id_view', $team_members_dropdown, $selected_team_member_id, 'class="form-control select2 w100p" id="team_member_id" disabled'); ?>
-                    </div>
+            <div class="form-group"><div class="row">
+                <label class="col-md-3"><?php echo app_lang('pontorh_employee'); ?></label>
+                <div class="col-md-9">
+                    <?php echo form_hidden('team_member_id', $selected_team_member_id); ?>
+                    <input type="text" class="form-control" value="<?php echo esc($record->team_member_name ?? '-'); ?>" disabled />
                 </div>
-            </div>
+            </div></div>
 
-            <div class="form-group">
-                <div class="row">
-                    <label for="work_date" class="col-md-3"><?php echo app_lang('pontorh_work_date'); ?></label>
-                    <div class="col-md-9">
-                        <input type="text" name="work_date" id="pontorh-record-work-date" class="form-control datepicker" value="<?php echo esc($selected_work_date); ?>" autocomplete="off" readonly required />
-                    </div>
-                </div>
-            </div>
+            <div class="form-group"><div class="row">
+                <label for="pontorh-record-work-date" class="col-md-3"><?php echo app_lang('pontorh_work_date'); ?></label>
+                <div class="col-md-9"><input type="text" name="work_date" id="pontorh-record-work-date" class="form-control datepicker" value="<?php echo esc($selected_work_date); ?>" autocomplete="off" readonly required /></div>
+            </div></div>
 
-            <div class="form-group">
-                <div class="row">
-                    <label for="punch_time" class="col-md-3"><?php echo app_lang('time'); ?></label>
-                    <div class="col-md-9">
-                        <input type="text" name="punch_time" id="pontorh-record-work-time" class="form-control timepicker" value="<?php echo esc($selected_punch_time); ?>" autocomplete="off" required />
-                    </div>
-                </div>
-            </div>
+            <div class="form-group"><div class="row">
+                <label for="pontorh-record-work-time" class="col-md-3"><?php echo app_lang('time'); ?></label>
+                <div class="col-md-9"><input type="text" name="punch_time" id="pontorh-record-work-time" class="form-control timepicker" value="<?php echo esc($selected_punch_time); ?>" autocomplete="off" required /></div>
+            </div></div>
 
-            <div class="form-group">
-                <div class="row">
-                    <label for="punch_type" class="col-md-3"><?php echo app_lang('pontorh_type'); ?></label>
-                    <div class="col-md-9">
-                        <?php echo form_dropdown('punch_type', $punch_type_dropdown, $selected_punch_type, 'class="form-control select2 w100p" id="punch_type" required'); ?>
-                    </div>
-                </div>
-            </div>
+            <div class="form-group"><div class="row">
+                <label for="pontorh-record-punch-type" class="col-md-3"><?php echo app_lang('pontorh_type'); ?></label>
+                <div class="col-md-9"><?php echo form_dropdown('punch_type', $punch_type_dropdown, $selected_punch_type, 'class="form-control" id="pontorh-record-punch-type" required'); ?></div>
+            </div></div>
 
-            <div class="form-group">
-                <div class="row">
-                    <label for="location_id" class="col-md-3"><?php echo app_lang('pontorh_location'); ?></label>
-                    <div class="col-md-9">
-                        <?php echo form_dropdown('location_id', $locations_dropdown, $selected_location_id, 'class="form-control select2 w100p" id="location_id"'); ?>
-                    </div>
-                </div>
-            </div>
+            <div class="form-group"><div class="row">
+                <label for="pontorh-record-location" class="col-md-3"><?php echo app_lang('pontorh_location'); ?></label>
+                <div class="col-md-9"><?php echo form_dropdown('location_id', $locations_dropdown, $selected_location_id, 'class="form-control" id="pontorh-record-location"'); ?></div>
+            </div></div>
 
-            <div class="form-group">
-                <div class="row">
-                    <label for="status" class="col-md-3"><?php echo app_lang('pontorh_status'); ?></label>
-                    <div class="col-md-9">
-                        <?php echo form_dropdown('status', $status_dropdown, $selected_status, 'class="form-control select2 w100p" id="status"'); ?>
-                    </div>
-                </div>
-            </div>
+            <div class="form-group"><div class="row">
+                <label for="pontorh-record-status" class="col-md-3"><?php echo app_lang('pontorh_status'); ?></label>
+                <div class="col-md-9"><?php echo form_dropdown('status', $status_dropdown, $selected_status, 'class="form-control" id="pontorh-record-status"'); ?></div>
+            </div></div>
 
-            <div class="form-group">
-                <div class="row">
-                    <label for="source" class="col-md-3"><?php echo app_lang('pontorh_source'); ?></label>
-                    <div class="col-md-9">
-                        <input type="text" name="source" id="source" class="form-control" value="<?php echo esc($selected_source); ?>" />
-                    </div>
-                </div>
-            </div>
+            <div class="form-group"><div class="row">
+                <label for="pontorh-record-source" class="col-md-3"><?php echo app_lang('pontorh_source'); ?></label>
+                <div class="col-md-9"><input type="text" name="source" id="pontorh-record-source" class="form-control" value="<?php echo esc($selected_source); ?>" /></div>
+            </div></div>
 
-            <div class="form-group">
-                <div class="row">
-                    <label for="latitude" class="col-md-3">Latitude</label>
-                    <div class="col-md-9">
-                        <input type="text" name="latitude" id="latitude" class="form-control" value="<?php echo esc($selected_latitude); ?>" />
-                    </div>
-                </div>
-            </div>
+            <div class="form-group"><div class="row">
+                <label for="pontorh-record-latitude" class="col-md-3">Latitude</label>
+                <div class="col-md-9"><input type="text" name="latitude" id="pontorh-record-latitude" class="form-control" value="<?php echo esc($selected_latitude); ?>" /></div>
+            </div></div>
 
-            <div class="form-group">
-                <div class="row">
-                    <label for="longitude" class="col-md-3">Longitude</label>
-                    <div class="col-md-9">
-                        <input type="text" name="longitude" id="longitude" class="form-control" value="<?php echo esc($selected_longitude); ?>" />
-                    </div>
-                </div>
-            </div>
+            <div class="form-group"><div class="row">
+                <label for="pontorh-record-longitude" class="col-md-3">Longitude</label>
+                <div class="col-md-9"><input type="text" name="longitude" id="pontorh-record-longitude" class="form-control" value="<?php echo esc($selected_longitude); ?>" /></div>
+            </div></div>
 
-            <div class="form-group">
-                <div class="row">
-                    <label for="notes" class="col-md-3"><?php echo app_lang('notes'); ?></label>
-                    <div class="col-md-9">
-                        <textarea name="notes" id="notes" class="form-control" rows="3"><?php echo esc($selected_notes); ?></textarea>
-                    </div>
-                </div>
-            </div>
+            <div class="form-group"><div class="row">
+                <label for="pontorh-record-notes" class="col-md-3"><?php echo app_lang('notes'); ?></label>
+                <div class="col-md-9"><textarea name="notes" id="pontorh-record-notes" class="form-control" rows="3"><?php echo esc($selected_notes); ?></textarea></div>
+            </div></div>
         <?php } ?>
 
-        <div class="form-group">
-            <div class="row">
-                <label for="justification" class="col-md-3"><?php echo app_lang('pontorh_record_action_reason'); ?></label>
-                <div class="col-md-9">
-                    <textarea name="justification" id="justification" class="form-control" rows="4" required></textarea>
-                </div>
-            </div>
-        </div>
+        <div class="form-group"><div class="row">
+            <label for="pontorh-record-justification" class="col-md-3"><?php echo app_lang('pontorh_record_action_reason'); ?></label>
+            <div class="col-md-9"><textarea name="justification" id="pontorh-record-justification" class="form-control" rows="4" required></textarea></div>
+        </div></div>
     </div>
 </div>
 <div class="modal-footer">
@@ -147,7 +105,6 @@ $selected_notes = (string) ($record->notes ?? '');
 
 <script type="text/javascript">
     $(document).ready(function () {
-        $("#pontorh-treatment-record-action-form .select2").select2();
         setDatePicker("#pontorh-record-work-date");
         setTimePicker("#pontorh-record-work-time");
 
